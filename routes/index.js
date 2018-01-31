124cd955b8e7037864dffba2559ccb72a32ede0c
Kotaro Seto <setok@oregonstate.edu>
2018-01-31 12:11:53 +0900

    Add gcloud settings

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  //res.render('index', { title: 'Express' });
+	var context = {
+		boat:'blah',
+		slip:'3',
+		date:'2017-03-19'
+	};
+  res.status(200).send(context);
 });
 
 module.exports = router;
